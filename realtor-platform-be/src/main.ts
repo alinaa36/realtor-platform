c396790bf7e5686e3c0f1d3f5a1799740e632689
alinaa36 <alinakulis8@gmail.com>
2024-05-24 01:14:38 +0300

    feat: ebabled cors

--- realtor-platform-be/src/main.ts
+++ realtor-platform-be/src/main.ts
@@ -4,6 +4,9 @@ import { ValidationPipe } from '@nestjs/common/pipes/validation.pipe';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors();
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
